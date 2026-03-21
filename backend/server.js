f9ca925aa4bc0d43b98d5ac4444592123c024de7
rohitmahto4420-afk <rohitmahto4420@gmail.com>
2026-03-20 22:31:47 -0700

    allchanges are done now all the components are working

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect(process.env.MONGO_URI)
+mongoose.connect("mongodb://jhaankush47_db_user:ankush123@ac-swslrtj-shard-00-00.wihifmz.mongodb.net:27017,ac-swslrtj-shard-00-01.wihifmz.mongodb.net:27017,ac-swslrtj-shard-00-02.wihifmz.mongodb.net:27017/?ssl=true&replicaSet=atlas-g0zg49-shard-0&authSource=admin&appName=ClusterTest")
   .then(() => console.log("MongoDB Connected"))
   .catch((err) => console.log(err));
 
@@ -25,6 +25,3 @@ app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on port ${PORT}`);
 });
 
-app.listen(PORT, () => {
-  console.log("Server running on port " + PORT);
-});
