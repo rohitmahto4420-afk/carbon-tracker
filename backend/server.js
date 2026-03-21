1ff3cbbcf08f89a8dda7c519ebdf1761a9a5c709
rohitmahto4420-afk <rohitmahto4420@gmail.com>
2026-03-20 22:57:51 -0700

    server.js is updated

--- backend/server.js
+++ backend/server.js
@@ -12,16 +12,17 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect("mongodb://jhaankush47_db_user:ankush123@ac-swslrtj-shard-00-00.wihifmz.mongodb.net:27017,ac-swslrtj-shard-00-01.wihifmz.mongodb.net:27017,ac-swslrtj-shard-00-02.wihifmz.mongodb.net:27017/?ssl=true&replicaSet=atlas-g0zg49-shard-0&authSource=admin&appName=ClusterTest")
-  .then(() => console.log("MongoDB Connected"))
-  .catch((err) => console.log(err));
+// ✅ Use ENV variable here
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB Connected ✅"))
+  .catch((err) => console.log("Mongo Error ❌:", err));
 
 app.use("/api/auth", authRoutes);
 app.use("/api/carbon", carbonRoutes);
 
+// ✅ Use ENV PORT
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on port ${PORT}`);
 });
-
